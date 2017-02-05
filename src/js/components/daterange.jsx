d3a400512f942c349225134144b2cf4968254a2c
jgoodhcg <jgoodhcg@gmail.com>
2017-02-05 10:29:54 -0500

    mousedown and up events bound

--- src/js/components/daterange.jsx
+++ src/js/components/daterange.jsx
@@ -7,30 +7,57 @@ export default class DateRange extends React.Component {
         super(props);
 
         this.state = {
+            id: this.props.idprefix,
             inactive: this.props.inactive,
             range: this.props.range,
             min: this.props.min,
             max: this.props.max,
             startUpdate: this.props.startUpdate,
-            endUpdate: this.props.endUpdate
+            endUpdate: this.props.endUpdate,
+            sx: 10, ex: 30
         };
+    }
 
+    componentDidMount(){
+        let start = document.getElementById(this.state.id+"-start");
+        let end   = document.getElementById(this.state.id+"-end");
+
+        start.addEventListener('mousedown',function(e){
+            console.log("down");
+            console.log(e);
+        });
+
+        start.addEventListener('mouseup',function(e){
+            console.log("up");
+            console.log(e);
+        });
 
     }
 
     render() {
-       return(
+
+        return(
             <svg class="skill" width="100%" height="100%" viewBox="0 0 100 10">
-                <line strokeLinecap="round"
-                      x1="10" x2="90" y1="5" y2="5"
-                      stroke={this.state.inactive} strokeWidth="2.5"/>
+                <line
+                    id={this.state.id + "-inactive"}
+                    strokeLinecap="round"
+                    x1="10" x2="90" y1="5" y2="5"
+                    stroke={this.state.inactive} strokeWidth="2.5"/>
 
-                <line strokeLinecap="round"
-                      x1="10"  x2="30" y1="5" y2="5"
-                      stroke={this.state.range} strokeWidth="2.5"/>
+                <line
+                    id={this.state.id + "-active"}
+                    strokeLinecap="round"
+                    x1="10"  x2="30" y1="5" y2="5"
+                    stroke={this.state.range} strokeWidth="2.5"/>
 
-                <circle cx="10" cy="5" r="5" fill={this.state.range}/>
-                <circle cx="30" cy="5" r="5" fill={this.state.range}/>
+                <circle
+                    id={this.state.id + "-start"}
+                    cx={this.state.sx}
+                    cy="5" r="5" fill={this.state.range}/>
+                <circle
+                    id={this.state.id + "-end"}
+                    cx={this.state.ex}
+                    cy="5" r="5" fill={this.state.range}/>
             </svg>
         );
     }
--- src/js/pages/exercise.jsx
+++ src/js/pages/exercise.jsx
@@ -17,6 +17,7 @@ export default class Exercise extends React.Component {
             <div>
                 hello world
                 <DateRange
+                    idprefix="date-range"
                     range="#68DADA" inactive="#989A9B"
                     min={new Date(2013, 1, 1)} max={Date.now()}
                     startUpdate={(t)=>{console.log("start is: "+t);}}
