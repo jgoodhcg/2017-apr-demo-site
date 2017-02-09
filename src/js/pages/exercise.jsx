7fdd6b7c304c6087fd4fcf1b23ec7226dd0465c5
jgoodhcg <jgoodhcg@gmail.com>
2017-02-09 07:15:43 -0500

    render x-axis w/ ticks

--- src/js/pages/exercise.jsx
+++ src/js/pages/exercise.jsx
@@ -15,11 +15,53 @@ export default class Exercise extends React.Component {
         this.min = d3.min(exercise_data, (day)=>{return day.start;});
         this.max = d3.max(exercise_data, (day)=>{return day.stop;});
 
-        console.log(exercise_data);
+        this.state = {
+            data: exercise_data
+        };
+
+        console.log(this.xTicks(this.state.data));
+    }
+
+    xTicks(data){
+        // return at most twenty dates to use as ticks
+        // bundled with the increment along x axis to render at
+        let pre_values = _(data)
+            .map((entry)=>{
+                let day = new Date(entry.start);
+                return day.getFullYear()+"-"
+                      +day.getMonth()+"-"
+                      +day.getDate();})
+            .uniq()
+            .value(),
+
+            max = 20,
+            num = (pre_values.length > max? max : pre_values.length),
+            increment = (num > max? 100/max : 100/num),
+            mod = Math.ceil(pre_values.length/num),
+
+            values = _(pre_values)
+                .filter((v,i)=>{return i % mod === 0;})
+                .map((value)=>{return {value, increment};})
+                .value();
+
+        return values;
+    }
+
+    renderXTick(x_t, index){
+        // expects x_t to be obj {value: val, increment: inc}
+        let x = x_t.increment * index;
+        return (
+            <line class="x-tick"
+                  x1={x} x2={x}
+                  y1="62.5" y2="61.5"
+                  stroke="black"
+                  strokeWidth="0.25"
+                  key={x_t.value}
+            />
+        );
     }
 
     render(){
-        let min = new Date(2013, 1, 1);
         return (
             <div>
                 <DateRange
@@ -29,6 +71,22 @@ export default class Exercise extends React.Component {
                     startUpdate={(t)=>{console.log("start is: "+t);}}
                     endUpdate={(t)=>{console.log("end is: "+t);}}
                 />
+            <svg width="100%" height="100%" viewBox="-10 -10 120 82.6">
+                <line
+                    class="x-axis"
+                    strokeLinecap="round"
+                    x1="0" x2="100" y1="62.5" y2="62.5"
+                    stroke="black" strokeWidth="0.25"/>
+
+                {this.xTicks(this.state.data).map(this.renderXTick.bind(this))}
+
+                <line
+                    class="y-axis"
+                    strokeLinecap="round"
+                    x1="0"  x2="0" y1="0" y2="62.5"
+                    stroke="black" strokeWidth="0.25"/>
+
+            </svg>
             </div>
         );
     }
