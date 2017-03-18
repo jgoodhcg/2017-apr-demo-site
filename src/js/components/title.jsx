b4facdf7362665324512451ee4dc566fe453a352
jgoodhcg <jgoodhcg@gmail.com>
2017-03-18 15:40:56 -0400

    added title to info

--- src/js/components/title.jsx
+++ src/js/components/title.jsx
@@ -17,6 +17,7 @@ export default class Activities extends React.Component {
                                 <InlineSVG src={require("./../../resources/justin.svg")} />
                             </div>
                             <div id="info" class="col-xs-12 col-sm-9">
+                                <h1 id="info-title" >Hi, I'm Justin</h1>
                                 <p id="bio" >
                                     Programmer, life quantifiying nut, wannabe artist. Since becoming
                                     and adult I've attempted to study movie making, illustration, and then
